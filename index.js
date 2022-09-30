7d84c1867707a26073119e050ac54830cf772ff2
hdbham <hunter.d.burningham@gmail.com>
2022-09-29 20:56:06 -0600

    adds profile information and repo information

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ class FetchWrapper {
         }).then(response => response.json());
     }
 }
+const profilePicture = document.querySelector("#profile-picture");
+const userInfo = document.querySelector("#user-info");
 
 const form = document.querySelector("#repos-form");
 const userName = document.querySelector("#github-username");
@@ -40,15 +42,27 @@ const GithubAPI = new FetchWrapper("https://api.github.com/users");
 form.addEventListener("submit", event => {
     event.preventDefault();
   
+    GithubAPI.get(`/${userName.value}`)
+    .then(data => {
+        userInfo.innerHTML = ""
+        userInfo.classList.add(`show`)
+        userInfo.insertAdjacentHTML("beforeend",`<img src="${data.avatar_url}">`) 
+        console.log(`${data}`);
+        userInfo.insertAdjacentHTML("beforeend",`<h1> ${data.name} </h1>`) 
+    })
+   
     GithubAPI.get(`/${userName.value}/repos`)
     .then(data =>  {
+        reposList.innerHTML = ""
         data.forEach(repo => {
+            console.log(repo)
             reposList.insertAdjacentHTML("beforeend", `<li>
-                <a href= "https://github.com/${repo.full_name}" target="_blank">
+                <a href="https://github.com/${repo.full_name}" target="_blank">
+                <p> ${repo.pushed_at.toString().substring(0,10)} </p>
                 <h2>${repo.full_name}</h2>
                 <p>${repo.description}</p>
                 </a>
                 </li>`);
         });
-    });
+    })
 });
